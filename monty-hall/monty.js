33150a0c916f98bc39eada72a4d4ce5f63cb8bfe
Wkamp <abscruffy@gmail.com>
2023-04-16 17:08:02 -0400

    Ugly but working, will probably  re-do without html canvas in the future

--- monty-hall/monty.js
+++ monty-hall/monty.js
@@ -4,6 +4,10 @@ const c = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 700;
 
+function randRange(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
 var mouseClick = {
     x: undefined,
     y: undefined
@@ -20,25 +24,25 @@ canvas.addEventListener("click", function(e) {
     var canvasY = Math.round(e.clientY - cRect.top);   // from the X/Y positions to make  
     mouseClick.x = canvasX;
     mouseClick.y = canvasY
-    //c.clearRect(0, 0, canvas.width, canvas.height);  // (0,0) the top left of the canvas
-    console.log("X: "+canvasX+", Y: "+canvasY);
 });
 
 class Rectangle {
-    constructor(x, y, width=50, height=100) {
+    constructor(x, y, width=50, height=100, color='rgb(139,69,19)') {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        this.color = 'rgb(139,69,19)';
+        this.color = color;
 
         this.treasure = false;
+        this.clickedOn = false;
     }
 
     draw() {
         c.beginPath();
         c.fillStyle = this.color;
         c.fillRect(this.x, this.y, this.width, this.height)
+
 
     }
     clicked() {
@@ -54,36 +58,98 @@ class Rectangle {
 }
 
 
-let door1;
-let door2;
-let door3;
-
 let doors;
 let picked;
+let switchYes;
+let switchNo;
+
+let treasureLocation;
+let gameOver;
+
+var totalSwitchedGames = 0;
+var totalSwitchWins = 0;
+
+var totalStayGames = 0;
+var totalStayWins = 0;
+
 function init() {
-    // door1 = new Rectangle(0,0);
-    // door2 = new Rectangle(100,0);
-    // door3 = new Rectangle(200,0);
-
     doors = [new Rectangle(0,0), new Rectangle(100,0), new Rectangle(200,0)];
+    treasureLocation = randRange(0,3);
+    doors[treasureLocation].treasure = true;
+    switchYes = new Rectangle(0,200, 70, 40, '#333');
+    switchNo = new Rectangle(100,200, 110, 40, '#333');
+    
+    gameOver = false;
+    picked = -1;
+}
 
-    picked = false;
+function stats() {
+    c.beginPath();
+    c.font = '20px Arial';
+    c.fillStyle = 'rgba(0,0,0,1)'
+    c.fillText('Stay Wins Percentage: ' + String(totalStayWins / totalStayGames * 100) + '%' ,0,300);
+    c.fillText('Switch Wins Percentage: ' + String(totalSwitchWins / totalSwitchedGames * 100) + '%' ,0,350);
 }
 
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
+    stats();
 
-    for (var i = 0; i < doors.length; i++) {
-        doors[i].draw();
+    //c.beginPath();
 
-        if (doors[i].clicked() && !picked) {
-            doors[i].color = 'rgba(139,69,19, 0.8)';
-            picked = true;
+    if (!gameOver) {
+        for (var i = 0; i < doors.length; i++) {
+            doors[i].draw();
+
+            if (doors[i].clicked() && picked == -1) {
+                doors[i].color = 'rgba(139,69,19, 0.8)';
+                doors[i].clickedOn = true;
+                picked = i;
+            }
         }
-    }
-    if (picked) {
+}
+    if (picked != -1 && !gameOver) {
+        stats();
+        console.log(picked, treasureLocation);
+        var hideDoor = 0;
+        while (hideDoor == picked || hideDoor == treasureLocation) {
+            hideDoor = randRange(0,3);
+        }
+        doors[hideDoor].color = 'white';
+        switchYes.draw();
+        c.font = '20px Arial'
+        c.fillStyle = 'white';
+        c.fillText('Switch',switchYes.x, switchYes.y + switchYes.height / 2)
 
+        switchNo.draw();
+        c.font = '20px Arial'
+        c.fillStyle = 'white';
+        c.fillText("don't switch",switchNo.x, switchNo.y + switchNo.height / 2)
+
+        if (switchYes.clicked()) {
+            totalSwitchedGames++
+            gameOver = true;
+            if (picked != treasureLocation) {
+                totalSwitchWins++
+                
+            }
+        }
+
+        if (switchNo.clicked()) {
+            totalStayGames++;
+            gameOver = true;
+            if (picked == treasureLocation) {
+                totalStayWins++;
+
+
+            }
+        }
+
+    }
+
+    if (gameOver) {
+        init();
     }
 }
 init();
