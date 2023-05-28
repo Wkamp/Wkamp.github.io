2f0c37d18f229f224506f8c35f5e23c8cceed0e1
Wkamp <abscruffy@gmail.com>
2023-05-28 17:07:46 -0400

    added graph link from main page

---
+++ Graph/graph.js
@@ -0,0 +1,15 @@
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    
+};
+
+animate();
